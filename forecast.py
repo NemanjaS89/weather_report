63ee463f4419a38882ade408cf08d3c0e47f24d4
NemanjaS89 <simic.nemanja1989@gmail.com>
2019-10-15 23:37:05 +0200

    added smtp

--- forecast.py
+++ forecast.py
@@ -3,10 +3,24 @@ from bs4 import BeautifulSoup
 
 response = requests.get('https://www.aladin.info/sr/bosna-i-hercegovina/brcko-dugorocna-prognoza-vremena')
 
-status = response.status_code
 soup = BeautifulSoup(response.text, 'lxml')
-
 tomorrow = soup.find_all('li', class_='font_150_rem text-primary')[2]
 
-print(status)
-print(tomorrow.text)
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    
+    server.login('simic.nemanja1989@gmail.com', '')
+    
+    subject = ''
+    body = ''
+    msg = f"Subject: {subject}\n\n{body}
+    
+    server.sendmail(
+            'simic.nemanja1989@gmail.com'
+            ''
+            ''
+            msg
+    )
