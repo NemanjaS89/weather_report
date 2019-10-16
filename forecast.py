03712410a966087864863c5e194a043407eaf547
NemanjaS89 <simic.nemanja1989@gmail.com>
2019-10-16 23:04:10 +0200

    imported environment variables; smtp confirmed working

--- forecast.py
+++ forecast.py
@@ -1,10 +1,18 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+
 
 response = requests.get('https://www.aladin.info/sr/bosna-i-hercegovina/brcko-dugorocna-prognoza-vremena')
 
 soup = BeautifulSoup(response.text, 'lxml')
 tomorrow = soup.find_all('li', class_='font_150_rem text-primary')[2]
+hladno = '0 stepeni'
+
+mail_user = os.environ.get('my_email')
+pass_user = os.environ.get('my_email_pass')
+
 
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -12,15 +20,17 @@ def send_mail():
     server.starttls()
     server.ehlo()
     
-    server.login('simic.nemanja1989@gmail.com', '')
+    server.login(mail_user, pass_user)
     
-    subject = ''
-    body = ''
-    msg = f"Subject: {subject}\n\n{body}
+    subject = 'Vremenska prognoza'
+    body = 'Ova poruka je automatski generisan izvjestaj!\n\nPrognozirana temperatura za sutra ujutro je {}'.format(hladno)
+    msg = f"Subject: {subject}\n\n{body}"
     
     server.sendmail(
-            'simic.nemanja1989@gmail.com'
-            ''
-            ''
+            mail_user,
+            mail_user,          
             msg
     )
+    
+
+send_mail()
