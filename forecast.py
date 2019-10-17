7846532dbe9bc33e6a5e8aecdc144140396f79b8
NemanjaS89 <simic.nemanja1989@gmail.com>
2019-10-17 23:53:47 +0200

    succesful string slicing

--- forecast.py
+++ forecast.py
@@ -8,11 +8,12 @@ response = requests.get('https://www.aladin.info/sr/bosna-i-hercegovina/brcko-du
 
 soup = BeautifulSoup(response.text, 'lxml')
 tomorrow = soup.find_all('li', class_='font_150_rem text-primary')[2]
-hladno = '0 stepeni'
+string = tomorrow.text
+sliced_string = string.split("°", 1)
+temperature = sliced_string[0]
 
 mail_user = os.environ.get('my_email')
 pass_user = os.environ.get('my_email_pass')
-
 
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -23,7 +24,7 @@ def send_mail():
     server.login(mail_user, pass_user)
     
     subject = 'Vremenska prognoza'
-    body = 'Ova poruka je automatski generisan izvjestaj!\n\nPrognozirana temperatura za sutra ujutro je {}'.format(hladno)
+    body = 'Ova poruka je automatski generisan izvjestaj!\n\nPrognozirana temperatura za sutra ujutro je {}'.format(temperature)
     msg = f"Subject: {subject}\n\n{body}"
     
     server.sendmail(
